f8c0fdfa3e954e1ac1294e9ba6ea5d8a2806d8c8
Gleb Uvarkin <gauvarkin@dev.vtb.ru>
2021-10-17 12:22:45 +0300

    Поправил замечания

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import './App.css';
-import {Counter} from "./Counter";
 import {Form} from "./Form/Form";
 
 
--- src/Form/InputForm.jsx
+++ src/Form/InputForm.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import {InputField} from "./InputField";
 
 export class InputForm extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return (
             <div>
