092354b048317609dd5a6360d23a9c2b88e3a83b
Gleb Uvarkin <gauvarkin@dev.vtb.ru>
2021-11-06 19:26:33 +0300

    Третья часть заключительного дз

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -1,17 +1,44 @@
-import {Component} from "react";
+import React, { useState, useEffect } from "react";
 import {Typography} from "antd";
+import {Octokit} from "@octokit/core";
 
-export class AboutPage extends Component {
-    render() {
-        return (
-            <div>
-                <Typography.Title>
-                    О нас
-                </Typography.Title>
-                <Typography.Paragraph>
-                    Скоро здесь появятся контакты.
-                </Typography.Paragraph>
-            </div>
-        )
-    }
+const {request} = new Octokit({auth: `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`});
+
+export function AboutPage() {
+    const githubProfileData = useGithubData('glebka35')
+    return (
+        <div>
+            <Typography.Title>
+                О нас
+            </Typography.Title>
+            <Typography.Paragraph>
+                Имя: {githubProfileData.name}
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                О себе: {githubProfileData.bio}
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                Почта: {githubProfileData.email}
+            </Typography.Paragraph>
+        </div>
+    )
+}
+
+function useGithubData(username) {
+    const [data, setData] = useState({
+        name: "",
+        bio: "",
+        email: ""
+    })
+    useEffect(() => {
+        const fetchData = async () => {
+            let result = await request('GET /users/' + username, {
+                username: 'username'
+            })
+
+            setData(result.data);
+        };
+        fetchData();
+    }, [username]);
+    return data
 }
